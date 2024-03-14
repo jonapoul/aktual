0850de34d040c4b8fd48f11642e4f9f6e81a7a4e
Jon Poulton <jpoulton@pm.me>
2024-03-14 08:39:01 +0000

    Build health

--- modules/core/ui/build.gradle.kts
+++ modules/core/ui/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
   implementation(libs.alakazam.android.compose)
   implementation(libs.androidx.coreKtx)
   implementation(libs.androidx.compose.foundation.core)
+  implementation(libs.androidx.compose.foundation.layout)
   implementation(libs.androidx.compose.ui.core)
   implementation(libs.androidx.compose.ui.graphics)
   implementation(libs.androidx.compose.ui.text)
--- modules/server-url/vm/build.gradle.kts
+++ modules/server-url/vm/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
   api(libs.androidx.lifecycle.viewmodel.ktx)
   api(libs.dagger.core)
   api(libs.javax.inject)
+  api(libs.kotlinx.coroutines)
   implementation(libs.alakazam.android.core)
   implementation(libs.hilt.android)
 }
