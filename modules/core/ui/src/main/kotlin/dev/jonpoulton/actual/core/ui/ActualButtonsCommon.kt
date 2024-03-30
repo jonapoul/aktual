0625fbb4b878d245666b2a716d829cfd429d7367
Jon Poulton <jpoulton@pm.me>
2024-03-30 12:33:08 +0000

    Add common icon buttons

---
+++ modules/core/ui/src/main/kotlin/dev/jonpoulton/actual/core/ui/ActualButtonsCommon.kt
@@ -0,0 +1,8 @@
+package dev.jonpoulton.actual.core.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+internal val ActualButtonPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp)
+internal val ActualButtonShape = RoundedCornerShape(size = 4.dp)
