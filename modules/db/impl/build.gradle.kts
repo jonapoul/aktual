f99a16dc008df1726bf96d533f1fd187f4d329d5
Jon Poulton <jpoulton@pm.me>
2024-04-02 21:18:09 +0100

    Bin off room

--- modules/db/impl/build.gradle.kts
+++ modules/db/impl/build.gradle.kts
@@ -1,20 +1,11 @@
 plugins {
   id("module-android")
-  alias(libs.plugins.ksp)
-  alias(libs.plugins.room)
 }
 
 android {
   namespace = "dev.jonpoulton.actual.db"
-
-  room {
-    schemaDirectory("$projectDir/schemas")
-  }
 }
 
 dependencies {
-  api(projects.modules.db.api)
-  api(libs.androidx.room.runtime)
-  api(libs.androidx.room.ktx)
-  ksp(libs.androidx.room.compiler)
+
 }
