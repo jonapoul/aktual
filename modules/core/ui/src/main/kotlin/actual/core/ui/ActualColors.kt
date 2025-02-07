6622b9476050d7554f2f0f199b02b1d8a64e510b
Jon Poulton <jpoulton@pm.me>
2025-02-07 21:55:48 +0000

    Refreshed colours

--- modules/core/ui/src/main/kotlin/actual/core/ui/ActualColors.kt
+++ modules/core/ui/src/main/kotlin/actual/core/ui/ActualColors.kt
@@ -2,8 +2,12 @@ package actual.core.ui
 
 import androidx.compose.ui.graphics.Color
 
+/**
+ * actual/packages/desktop-client/src/style/palette.ts
+ */
 object ActualColors {
   val gray50 = Color(color = 0xFFF6F8FA)
+  val gray80 = Color(color = 0xFFF0F4F6)
   val gray100 = Color(color = 0xFFE8ECF0)
   val gray150 = Color(color = 0xFFD4DAE0)
   val gray200 = Color(color = 0xFFBDC5CF)
@@ -14,73 +18,149 @@ object ActualColors {
   val gray700 = Color(color = 0xFF242733)
   val gray800 = Color(color = 0xFF141520)
   val gray900 = Color(color = 0xFF080811)
-  val navy50 = Color(color = 0xFFF7FAFC)
-  val navy100 = Color(color = 0xFFE8ECF0)
-  val navy150 = Color(color = 0xFFD9E2EC)
-  val navy200 = Color(color = 0xFFBCCCDC)
-  val navy300 = Color(color = 0xFF9FB3C8)
-  val navy400 = Color(color = 0xFF829AB1)
-  val navy500 = Color(color = 0xFF627D98)
-  val navy600 = Color(color = 0xFF486581)
-  val navy700 = Color(color = 0xFF334E68)
-  val navy800 = Color(color = 0xFF243B53)
-  val navy900 = Color(color = 0xFF102A43)
-  val blue50 = Color(color = 0xFFF5FCFF)
-  val blue100 = Color(color = 0xFFE3F0FF)
-  val blue150 = Color(color = 0xFFB3D9FF)
-  val blue200 = Color(color = 0xFF8BCAFD)
-  val blue300 = Color(color = 0xFF66B5FA)
-  val blue400 = Color(color = 0xFF40A5F7)
-  val blue500 = Color(color = 0xFF2B8FED)
-  val blue600 = Color(color = 0xFF1980D4)
-  val blue700 = Color(color = 0xFF1271BF)
-  val blue800 = Color(color = 0xFF0B5FA3)
-  val blue900 = Color(color = 0xFF034388)
-  val green50 = Color(color = 0xFFFAFFFD)
-  val green100 = Color(color = 0xFFEFFCF6)
-  val green150 = Color(color = 0xFFC6F7E2)
-  val green200 = Color(color = 0xFF8EEDC7)
-  val green300 = Color(color = 0xFF65D6AD)
-  val green400 = Color(color = 0xFF3EBD93)
-  val green500 = Color(color = 0xFF27AB83)
-  val green600 = Color(color = 0xFF199473)
-  val green700 = Color(color = 0xFF147D64)
-  val green800 = Color(color = 0xFF0C6B58)
-  val green900 = Color(color = 0xFF014D40)
-  val orange50 = Color(color = 0xFFFFFEFA)
-  val orange100 = Color(color = 0xFFFFFBEA)
-  val orange150 = Color(color = 0xFFFFF7C4)
-  val orange200 = Color(color = 0xFFFCF088)
-  val orange300 = Color(color = 0xFFF5E35D)
-  val orange400 = Color(color = 0xFFF2D047)
-  val orange500 = Color(color = 0xFFE6BB20)
-  val orange600 = Color(color = 0xFFD4A31C)
-  val orange700 = Color(color = 0xFFB88115)
-  val orange800 = Color(color = 0xFF87540D)
-  val orange900 = Color(color = 0xFF733309)
-  val red50 = Color(color = 0xFFFFF1F1)
-  val red100 = Color(color = 0xFFFFE3E3)
-  val red150 = Color(color = 0xFFFFBDBD)
-  val red200 = Color(color = 0xFFFF9B9B)
-  val red300 = Color(color = 0xFFF86A6A)
-  val red400 = Color(color = 0xFFEF4E4E)
-  val red500 = Color(color = 0xFFE12D39)
-  val red600 = Color(color = 0xFFCF1124)
-  val red700 = Color(color = 0xFFAB091E)
-  val red800 = Color(color = 0xFF8A041A)
-  val red900 = Color(color = 0xFF610316)
-  val purple50 = Color(color = 0xFFF9F6FE)
-  val purple100 = Color(color = 0xFFF2EBFE)
-  val purple150 = Color(color = 0xFFDAC4FF)
-  val purple200 = Color(color = 0xFFB990FF)
-  val purple300 = Color(color = 0xFFA368FC)
-  val purple400 = Color(color = 0xFF9446ED)
-  val purple500 = Color(color = 0xFF8719E0)
-  val purple600 = Color(color = 0xFF7A0ECC)
-  val purple700 = Color(color = 0xFF690CB0)
-  val purple800 = Color(color = 0xFF580A94)
-  val purple900 = Color(color = 0xFF44056E)
-  val white = Color(color = 0xFFFFFFFF)
+  val navy50 = OldColors.n11
+  val navy100 = OldColors.n10
+  val navy150 = OldColors.n9
+  val navy200 = OldColors.n8
+  val navy300 = OldColors.n7
+  val navy400 = OldColors.n6
+  val navy500 = OldColors.n5
+  val navy600 = OldColors.n4
+  val navy700 = OldColors.n3
+  val navy800 = OldColors.n2
+  val navy900 = OldColors.n1
+  val blue50 = OldColors.b11
+  val blue100 = OldColors.b10
+  val blue150 = OldColors.b9
+  val blue200 = OldColors.b8
+  val blue300 = OldColors.b7
+  val blue400 = OldColors.b6
+  val blue500 = OldColors.b5
+  val blue600 = OldColors.b4
+  val blue700 = OldColors.b3
+  val blue800 = OldColors.b2
+  val blue900 = OldColors.b1
+  val green50 = OldColors.g11
+  val green100 = OldColors.g10
+  val green150 = OldColors.g9
+  val green200 = OldColors.g8
+  val green300 = OldColors.g7
+  val green400 = OldColors.g6
+  val green500 = OldColors.g5
+  val green600 = OldColors.g4
+  val green700 = OldColors.g3
+  val green800 = OldColors.g2
+  val green900 = OldColors.g1
+  val orange50 = OldColors.y11
+  val orange100 = OldColors.y10
+  val orange150 = OldColors.y9
+  val orange200 = OldColors.y8
+  val orange300 = OldColors.y7
+  val orange400 = OldColors.y6
+  val orange500 = OldColors.y5
+  val orange600 = OldColors.y4
+  val orange700 = OldColors.y3
+  val orange800 = OldColors.y2
+  val orange900 = OldColors.y1
+  val red50 = OldColors.r11
+  val red100 = OldColors.r10
+  val red150 = OldColors.r9
+  val red200 = OldColors.r8
+  val red300 = OldColors.r7
+  val red400 = OldColors.r6
+  val red500 = OldColors.r5
+  val red600 = OldColors.r4
+  val red700 = OldColors.r3
+  val red800 = OldColors.r2
+  val red900 = OldColors.r1
+  val purple50 = OldColors.p11
+  val purple100 = OldColors.p10
+  val purple150 = OldColors.p9
+  val purple200 = OldColors.p8
+  val purple300 = OldColors.p7
+  val purple400 = OldColors.p6
+  val purple500 = OldColors.p5
+  val purple600 = OldColors.p4
+  val purple700 = OldColors.p3
+  val purple800 = OldColors.p2
+  val purple900 = OldColors.p1
+  val white = Color.White
+  val black = Color.Black
+  val hover = OldColors.hover
+}
+
+/**
+ * actual/packages/desktop-client/src/style/colors.ts
+ */
+private object OldColors {
+  val y1 = Color(color = 0xFF733309)
+  val y2 = Color(color = 0xFF87540D)
+  val y3 = Color(color = 0xFFB88115)
+  val y4 = Color(color = 0xFFD4A31C)
+  val y5 = Color(color = 0xFFE6BB20)
+  val y6 = Color(color = 0xFFF2D047)
+  val y7 = Color(color = 0xFFF5E35D)
+  val y8 = Color(color = 0xFFFCF088)
+  val y9 = Color(color = 0xFFFFF7C4)
+  val y10 = Color(color = 0xFFFFFBEA)
+  val y11 = Color(color = 0xFFFFFEFA)
+  val r1 = Color(color = 0xFF610316)
+  val r2 = Color(color = 0xFF8A041A)
+  val r3 = Color(color = 0xFFAB091E)
+  val r4 = Color(color = 0xFFCF1124)
+  val r5 = Color(color = 0xFFE12D39)
+  val r6 = Color(color = 0xFFEF4E4E)
+  val r7 = Color(color = 0xFFF86A6A)
+  val r8 = Color(color = 0xFFFF9B9B)
+  val r9 = Color(color = 0xFFFFBDBD)
+  val r10 = Color(color = 0xFFFFE3E3)
+  val r11 = Color(color = 0xFFFFF1F1)
+  val b1 = Color(color = 0xFF034388)
+  val b2 = Color(color = 0xFF0B5FA3)
+  val b3 = Color(color = 0xFF1271BF)
+  val b4 = Color(color = 0xFF1980D4)
+  val b5 = Color(color = 0xFF2B8FED)
+  val b6 = Color(color = 0xFF40A5F7)
+  val b7 = Color(color = 0xFF66B5FA)
+  val b8 = Color(color = 0xFF8BCAFD)
+  val b9 = Color(color = 0xFFB3D9FF)
+  val b10 = Color(color = 0xFFE3F0FF)
+  val b11 = Color(color = 0xFFF5FCFF)
+  val n1 = Color(color = 0xFF102A43)
+  val n2 = Color(color = 0xFF243B53)
+  val n3 = Color(color = 0xFF334E68)
+  val n4 = Color(color = 0xFF486581)
+  val n5 = Color(color = 0xFF627D98)
+  val n6 = Color(color = 0xFF829AB1)
+  val n7 = Color(color = 0xFF9FB3C8)
+  val n8 = Color(color = 0xFFBCCCDC)
+  val n9 = Color(color = 0xFFD9E2EC)
+  val n10 = Color(color = 0xFFE8ECF0)
+  val n11 = Color(color = 0xFFF7FAFC)
+  val g1 = Color(color = 0xFF014D40)
+  val g2 = Color(color = 0xFF0C6B58)
+  val g3 = Color(color = 0xFF147D64)
+  val g4 = Color(color = 0xFF199473)
+  val g5 = Color(color = 0xFF27AB83)
+  val g6 = Color(color = 0xFF3EBD93)
+  val g7 = Color(color = 0xFF65D6AD)
+  val g8 = Color(color = 0xFF8EEDC7)
+  val g9 = Color(color = 0xFFC6F7E2)
+  val g10 = Color(color = 0xFFEFFCF6)
+  val g11 = Color(color = 0xFFFAFFFD)
+  val p1 = Color(color = 0xFF44056E)
+  val p2 = Color(color = 0xFF580A94)
+  val p3 = Color(color = 0xFF690CB0)
+  val p4 = Color(color = 0xFF7A0ECC)
+  val p5 = Color(color = 0xFF8719E0)
+  val p6 = Color(color = 0xFF9446ED)
+  val p7 = Color(color = 0xFFA368FC)
+  val p8 = Color(color = 0xFFB990FF)
+  val p9 = Color(color = 0xFFDAC4FF)
+  val p10 = Color(color = 0xFFF2EBFE)
+  val p11 = Color(color = 0xFFF9F6FE)
+
+  val border = n10
   val hover = Color(color = 0xFFFAFAFA)
-  val black = Color(color = 0xFF000000)
+  val selected = b9
 }
