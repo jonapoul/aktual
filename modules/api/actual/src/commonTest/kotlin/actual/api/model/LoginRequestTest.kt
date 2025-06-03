75069e1577a72ee7066271b264f089cbc8e46d38
Jon Poulton <jpoulton@pm.me>
2025-06-03 17:19:59 +0100

    Create LoginRequestTest.kt

---
+++ modules/api/actual/src/commonTest/kotlin/actual/api/model/LoginRequestTest.kt
@@ -0,0 +1,30 @@
+package actual.api.model
+
+import actual.account.model.LoginMethod
+import actual.account.model.Password
+import actual.api.model.account.LoginRequest
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class LoginRequestTest {
+  val request = LoginRequest(
+    loginMethod = LoginMethod.Password,
+    password = Password("P@ssw0rd!"),
+  )
+
+  @Test
+  fun `Convert to string with blocked out password`() {
+    assertEquals(
+      expected = "LoginRequest(loginMethod=Password, password=■■■)",
+      actual = request.toString(),
+    )
+  }
+  @Test
+  fun `Serialize to json without blocking out password`() {
+    assertEquals(
+      expected = """{"loginMethod":"password","password":"P@ssw0rd!"}""",
+      actual = Json.encodeToString(request),
+    )
+  }
+}
