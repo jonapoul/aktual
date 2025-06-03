722594ce736876a6359f58f91589f17a5930d414
Jon Poulton <jpoulton@pm.me>
2025-06-03 17:16:18 +0100

    Add to data class params

--- modules/api/actual/src/commonMain/kotlin/actual/api/model/account/ChangePasswordRequest.kt
+++ modules/api/actual/src/commonMain/kotlin/actual/api/model/account/ChangePasswordRequest.kt
@@ -4,9 +4,10 @@ import actual.account.model.LoginToken
 import actual.account.model.Password
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import net.afanasev.sekret.Secret
 
 @Serializable
 data class ChangePasswordRequest(
   @SerialName("token") val token: LoginToken,
-  @SerialName("password") val password: Password,
+  @Secret @SerialName("password") val password: Password,
 )
