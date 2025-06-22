729c397ef79780336e6853359a58bda9b434049a
Jon Poulton <jpoulton@pm.me>
2025-06-22 14:25:33 +0100

    Added Localization

---
+++ modules/l10n/src/androidMain/kotlin/actual/l10/AndroidLocalization.kt
@@ -0,0 +1,11 @@
+package actual.l10
+
+import android.content.Context
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+class AndroidLocalization(private val context: Context) : Localization {
+  override fun dimen(id: Int): Dp = context.resources.getDimension(id).dp
+  override fun string(id: Int): String = context.getString(id)
+  override fun string(id: Int, vararg args: Any): String = context.getString(id, *args)
+}
---
+++ modules/l10n/src/commonMain/kotlin/actual/l10/ComposableLocalization.kt
@@ -0,0 +1,6 @@
+package actual.l10
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun l10n(): Localization
