558e9e7ecab82d00e8e9b715d57c923b05fc3b9b
Jon Poulton <jpoulton@pm.me>
2025-06-25 18:44:11 +0100

    Remove sekret

--- modules/api/actual/src/commonMain/kotlin/actual/api/model/account/LoginRequest.kt
+++ modules/api/actual/src/commonMain/kotlin/actual/api/model/account/LoginRequest.kt
@@ -4,10 +4,9 @@ import actual.account.model.LoginMethod
 import actual.account.model.Password
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import net.afanasev.sekret.Secret
 
 @Serializable
 data class LoginRequest(
   @SerialName("loginMethod") val loginMethod: LoginMethod,
-  @Secret @SerialName("password") val password: Password,
+  @SerialName("password") val password: Password,
 )
