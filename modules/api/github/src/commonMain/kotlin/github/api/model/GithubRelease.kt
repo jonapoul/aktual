47af64e821799e95e276d950612835d0484cfa33
Jon Poulton <jpoulton@pm.me>
2025-06-27 16:17:32 +0100

    Bumped

--- modules/api/github/src/commonMain/kotlin/github/api/model/GithubRelease.kt
+++ modules/api/github/src/commonMain/kotlin/github/api/model/GithubRelease.kt
@@ -1,6 +1,5 @@
 package github.api.model
 
-import actual.core.model.InstantSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.time.Instant
@@ -14,7 +13,7 @@ data class GithubRelease(
   @SerialName("name") val versionName: String,
 
   // The ISO-8601 string timestamp of the publish. E.g. "2021-11-06T12:15:10Z"
-  @SerialName("published_at") @Serializable(InstantSerializer::class) val publishedAt: Instant,
+  @SerialName("published_at") val publishedAt: Instant,
 
   // The URL of the release tag. E.g. "https://github.com/jonapoul/actual-android/releases/tag/1.0.0"
   @SerialName("html_url") val htmlUrl: String,
