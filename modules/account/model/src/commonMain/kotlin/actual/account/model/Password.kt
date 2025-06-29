0ab6e3832f0d0062665c11e66afae0d83a8baf1e
Jon Poulton <jpoulton@pm.me>
2025-06-29 12:55:33 +0100

    Re-add CharSequence

--- modules/account/model/src/commonMain/kotlin/actual/account/model/Password.kt
+++ modules/account/model/src/commonMain/kotlin/actual/account/model/Password.kt
@@ -2,11 +2,15 @@ package actual.account.model
 
 import dev.zacsweers.redacted.annotations.Redacted
 import kotlinx.serialization.Serializable
+import java.util.stream.IntStream
 
 @Redacted
 @JvmInline
 @Serializable
-value class Password(val value: String) {
+value class Password(val value: String) : CharSequence by value {
+  override fun chars(): IntStream = value.chars()
+  override fun codePoints(): IntStream = value.codePoints()
+
   fun interface Provider {
     fun default(): Password
   }
