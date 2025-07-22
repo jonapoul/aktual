dcf9e2efdffcb52b8cbf62e6c180e304741be033
Jon Poulton <jpoulton@pm.me>
2025-07-22 18:11:12 +0100

    detektAll -> detektCheck (#407)

--- build-logic/src/main/kotlin/actual/gradle/ConventionDetekt.kt
+++ build-logic/src/main/kotlin/actual/gradle/ConventionDetekt.kt
@@ -2,18 +2,45 @@ package actual.gradle
 
 import blueprint.core.getLibrary
 import blueprint.core.libs
-import blueprint.recipes.DetektAll
-import blueprint.recipes.detektBlueprint
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.getting
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.registering
+import org.gradle.kotlin.dsl.withType
 
 class ConventionDetekt : Plugin<Project> {
   override fun apply(target: Project) = with(target) {
-    detektBlueprint(
-      configFile = rootProject.file("config/detekt.yml"),
-      detektAllConfig = DetektAll.Apply(ignoreRelease = true),
-    )
+    with(plugins) {
+      apply(DetektPlugin::class)
+    }
+
+    extensions.getByType(DetektExtension::class).apply {
+      config.setFrom(rootProject.file("config/detekt.yml"))
+      buildUponDefaultConfig = true
+    }
+
+    val check by tasks.getting
+    val detektTasks = tasks.withType<Detekt>()
+    val detektCheck by tasks.registering
+
+    detektCheck.configure { dependsOn(detektTasks) }
+
+    detektTasks.configureEach {
+      reports.html.required.set(true)
+      exclude { it.file.path.contains("generated") }
+
+      if (!name.contains("release", ignoreCase = true)) {
+        check.dependsOn(this)
+      }
+    }
 
     dependencies {
       "detektPlugins"(libs.getLibrary("plugin.detektCompose"))
