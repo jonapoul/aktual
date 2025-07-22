d6b2c85080fd2d60622d5f46f037d2c30723f495
Jon Poulton <jpoulton@pm.me>
2025-07-22 18:07:53 +0100

    Update

--- modules/budget/reports/ui/src/main/kotlin/actual/budget/reports/ui/Previews.kt
+++ modules/budget/reports/ui/src/main/kotlin/actual/budget/reports/ui/Previews.kt
@@ -1,6 +1,6 @@
 package actual.budget.reports.ui
 
-import actual.budget.model.CustomReportId
+import actual.budget.model.WidgetId
 import actual.budget.reports.ui.charts.PreviewCashFlow
 import actual.budget.reports.ui.charts.PreviewNetWorth
 import actual.budget.reports.ui.charts.PreviewSummary
@@ -12,19 +12,19 @@ internal fun date(year: Int, month: Month) = YearMonth(year, month)
 
 internal object ReportDashboardItems {
   internal val ITEM_1 = ReportDashboardItem(
-    id = CustomReportId("abc-123"),
+    id = WidgetId("abc-123"),
     name = "Pensions",
     data = PreviewCashFlow.DATA,
   )
 
   internal val ITEM_2 = ReportDashboardItem(
-    id = CustomReportId("def-456"),
+    id = WidgetId("def-456"),
     name = "Groceries",
     data = PreviewNetWorth.DATA,
   )
 
   internal val ITEM_3 = ReportDashboardItem(
-    id = CustomReportId("xyz-789"),
+    id = WidgetId("xyz-789"),
     name = "Pensions",
     data = PreviewSummary.PER_TRANSACTION_DATA,
   )
--- modules/budget/reports/vm/src/main/kotlin/actual/budget/reports/vm/ReportDashboardItem.kt
+++ modules/budget/reports/vm/src/main/kotlin/actual/budget/reports/vm/ReportDashboardItem.kt
@@ -1,11 +1,11 @@
 package actual.budget.reports.vm
 
-import actual.budget.model.CustomReportId
+import actual.budget.model.WidgetId
 import androidx.compose.runtime.Immutable
 
 @Immutable
 data class ReportDashboardItem(
-  val id: CustomReportId,
+  val id: WidgetId,
   val name: String,
   val data: ChartData,
 )
