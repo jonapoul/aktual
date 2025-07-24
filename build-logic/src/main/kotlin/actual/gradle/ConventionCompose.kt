38e0ebe5e197ad2c11bba798fb118a1c15186a83
Jon Poulton <jpoulton@pm.me>
2025-07-24 19:23:35 +0100

    Remove composeBlueprint

--- build-logic/src/main/kotlin/actual/gradle/ConventionCompose.kt
+++ build-logic/src/main/kotlin/actual/gradle/ConventionCompose.kt
@@ -1,27 +1,43 @@
 package actual.gradle
 
 import blueprint.core.getLibrary
-import blueprint.core.getVersion
 import blueprint.core.libs
-import blueprint.recipes.composeBlueprint
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradleSubplugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.androidJvm
 
 class ConventionCompose : Plugin<Project> {
   override fun apply(target: Project) = with(target) {
     with(pluginManager) {
-      apply(ConventionKotlinJvm::class.java)
+      apply(ConventionKotlinBase::class.java)
       apply(ConventionAndroidBase::class.java)
+      apply(ComposeCompilerGradleSubplugin::class)
     }
 
-    composeBlueprint(
-      composeBomVersion = libs.getVersion("androidx.compose.bom"),
-      composeLintVersion = libs.getVersion("androidx.compose.lint"),
-      writeMetrics = true,
-      experimentalApis = emptyList(),
-      stabilityFile = rootProject.layout.projectDirectory.file("config/compose-stability.conf"),
-    )
+    extensions.getByType(CommonExtension::class).apply {
+      buildFeatures {
+        compose = true
+      }
+    }
+
+    val metricReportDir = project.layout.buildDirectory.dir("compose_metrics")
+    val stabilityFile = rootProject.layout.projectDirectory.file("config/compose-stability.conf")
+
+    extensions.configure<ComposeCompilerGradlePluginExtension> {
+      metricsDestination.set(metricReportDir)
+      reportsDestination.set(metricReportDir)
+
+      stabilityConfigurationFiles.add(stabilityFile)
+
+      targetKotlinPlatforms.addAll(androidJvm)
+    }
 
     dependencies {
       "implementation"(platform(libs.getLibrary("androidx.compose.bom")))
