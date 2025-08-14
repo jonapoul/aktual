fda0d97631019435d7edc4d10edda0891c820ecd
Jon Poulton <jpoulton@pm.me>
2025-08-14 18:15:54 +0100

    Update

--- build-logic/src/main/kotlin/actual/gradle/TestLibs.kt
+++ build-logic/src/main/kotlin/actual/gradle/TestLibs.kt
@@ -21,11 +21,11 @@ internal val Project.testLibraries: List<Provider<MinimalExternalModuleDependenc
 internal val Project.androidTestLibraries: List<Provider<MinimalExternalModuleDependency>>
   get() = with(libs) {
     listOf(
-      libs.getLibrary("test.androidx.arch"),
-      libs.getLibrary("test.androidx.coreKtx"),
-      libs.getLibrary("test.androidx.junit"),
-      libs.getLibrary("test.androidx.rules"),
-      libs.getLibrary("test.androidx.runner"),
-      libs.getLibrary("test.robolectric"),
+      getLibrary("test.androidx.arch"),
+      getLibrary("test.androidx.coreKtx"),
+      getLibrary("test.androidx.junit"),
+      getLibrary("test.androidx.rules"),
+      getLibrary("test.androidx.runner"),
+      getLibrary("test.robolectric"),
     )
   }
