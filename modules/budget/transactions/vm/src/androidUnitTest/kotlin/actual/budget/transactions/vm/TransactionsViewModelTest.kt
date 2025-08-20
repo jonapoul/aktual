fb0de5740fe75514b602d850436378a16bc4036f
Jon Poulton <jpoulton@pm.me>
2025-08-20 22:52:15 +0100

    Close database in unit tests when finished (#479)

--- modules/budget/transactions/vm/src/androidUnitTest/kotlin/actual/budget/transactions/vm/TransactionsViewModelTest.kt
+++ modules/budget/transactions/vm/src/androidUnitTest/kotlin/actual/budget/transactions/vm/TransactionsViewModelTest.kt
@@ -3,19 +3,15 @@ package actual.budget.transactions.vm
 import actual.app.di.AndroidViewModelGraph
 import actual.app.di.CoroutineContainer
 import actual.app.di.GithubApiContainer
-import actual.budget.db.AndroidSqlDriverFactory
-import actual.budget.db.BudgetDatabase
-import actual.budget.db.buildDatabase
 import actual.budget.model.AccountId
 import actual.budget.model.AccountSpec
 import actual.budget.model.AccountSpec.AllAccounts
 import actual.budget.model.AccountSpec.SpecificAccount
-import actual.budget.model.AndroidBudgetFiles
-import actual.budget.model.BudgetFiles
 import actual.budget.model.CategoryId
 import actual.budget.model.PayeeId
 import actual.budget.model.TransactionsSpec
 import actual.core.di.AppGraph
+import actual.core.di.BudgetGraph
 import actual.core.di.BudgetGraphHolder
 import actual.core.di.assisted
 import actual.test.DummyViewModelContainer
@@ -24,7 +20,6 @@ import alakazam.test.core.TestCoroutineContexts
 import android.content.Context
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.test.core.app.ApplicationProvider
-import app.cash.sqldelight.db.SqlDriver
 import app.cash.turbine.test
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
@@ -37,7 +32,6 @@ import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import okio.FileSystem
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import kotlin.test.AfterTest
@@ -49,32 +43,32 @@ import kotlin.test.assertEquals
 class TransactionsViewModelTest : AppGraph.Holder {
   // real
   private lateinit var viewModel: TransactionsViewModel
-  private lateinit var fileSystem: FileSystem
-  private lateinit var driver: SqlDriver
+  private lateinit var budgetGraph: BudgetGraph
 
   // fake
   private lateinit var appGraph: TestAppGraph
-  private lateinit var files: BudgetFiles
-  private lateinit var database: BudgetDatabase
 
   override fun invoke(): AppGraph = appGraph
 
   @AfterTest
   fun after() {
-    driver.close()
+    budgetGraph.driver.close()
   }
 
   private suspend fun TestScope.buildViewModel(spec: AccountSpec) {
     val context = ApplicationProvider.getApplicationContext<Context>()
-    fileSystem = FileSystem.SYSTEM
-    files = AndroidBudgetFiles(context, fileSystem)
 
-    val factory = AndroidSqlDriverFactory(BUDGET_ID, context, files)
-    driver = factory.create()
-    database = buildDatabase(driver)
+    appGraph = createGraphFactory<TestAppGraph.Factory>().create(
+      scope = this,
+      contexts = TestCoroutineContexts(StandardTestDispatcher(testScheduler)),
+      context = context,
+      holder = this@TransactionsViewModelTest,
+    )
+
+    budgetGraph = appGraph.budgetGraphHolder.update(METADATA)
 
     // add some utility entities
-    with(database) {
+    with(budgetGraph.database) {
       insertAccount(AccountId("a"), "Amex")
       insertAccount(AccountId("b"), "Barclays")
       insertAccount(AccountId("c"), "Chase")
@@ -87,15 +81,6 @@ class TransactionsViewModelTest : AppGraph.Holder {
       insertCategory(CategoryId("b"), "Building")
       insertCategory(CategoryId("c"), "Car")
     }
-
-    appGraph = createGraphFactory<TestAppGraph.Factory>().create(
-      scope = this,
-      contexts = TestCoroutineContexts(StandardTestDispatcher(testScheduler)),
-      context = context,
-      holder = this@TransactionsViewModelTest,
-    )
-
-    appGraph.budgetGraphHolder.update(METADATA)
 
     viewModel = appGraph
       .create(CreationExtras.Empty)
@@ -123,7 +108,7 @@ class TransactionsViewModelTest : AppGraph.Holder {
   fun `Transactions from all accounts grouped in one date`() = runTest {
     // given
     buildViewModel(AllAccounts)
-    with(database) {
+    with(budgetGraph.database) {
       insertTransaction(id = "a", account = "a", category = "a", payee = "a")
       insertTransaction(id = "b", account = "b", category = "b", payee = "b")
       insertTransaction(id = "c", account = "c", category = "c", payee = "c")
@@ -148,7 +133,7 @@ class TransactionsViewModelTest : AppGraph.Holder {
   fun `Transactions from one account`() = runTest {
     // given
     buildViewModel(SpecificAccount(AccountId("a")))
-    with(database) {
+    with(budgetGraph.database) {
       insertTransaction(id = "a", account = "a", category = "a", payee = "a") // included
       insertTransaction(id = "b", account = "b", category = "b", payee = "b") // ignored
       insertTransaction(id = "c", account = "c", category = "c", payee = "c") // ignored
@@ -171,7 +156,7 @@ class TransactionsViewModelTest : AppGraph.Holder {
   fun `Transactions grouped by date`() = runTest {
     // given
     buildViewModel(AllAccounts)
-    with(database) {
+    with(budgetGraph.database) {
       insertTransaction(id = "a", account = "a", category = "a", payee = "a", date = DATE_1)
       insertTransaction(id = "b", account = "b", category = "b", payee = "b", date = DATE_1)
       insertTransaction(id = "c", account = "c", category = "c", payee = "c", date = DATE_1)
@@ -197,21 +182,21 @@ class TransactionsViewModelTest : AppGraph.Holder {
     }
   }
 
-//  @Test
-//  fun `Observing transaction`() = runTest {
-//    // given
-//    buildViewModel(AllAccounts)
-//    with(database) {
-//      insertTransaction(id = ID_A.toString(), account = "a", category = "a", payee = "a", date = DATE_1)
-//      insertTransaction(id = ID_B.toString(), account = "b", category = "b", payee = "b", date = DATE_2)
-//      insertTransaction(id = ID_C.toString(), account = "c", category = "c", payee = "c", date = DATE_3)
-//    }
-//    advanceUntilIdle()
-//
-//    assertEquals(actual = viewModel.observe(ID_A).first(), expected = TRANSACTION_A)
-//    assertEquals(actual = viewModel.observe(ID_B).first(), expected = TRANSACTION_B)
-//    assertEquals(actual = viewModel.observe(ID_C).first(), expected = TRANSACTION_C)
-//  }
+  //  @Test
+  //  fun `Observing transaction`() = runTest {
+  //    // given
+  //    buildViewModel(AllAccounts)
+  //    with(database) {
+  //      insertTransaction(id = ID_A.toString(), account = "a", category = "a", payee = "a", date = DATE_1)
+  //      insertTransaction(id = ID_B.toString(), account = "b", category = "b", payee = "b", date = DATE_2)
+  //      insertTransaction(id = ID_C.toString(), account = "c", category = "c", payee = "c", date = DATE_3)
+  //    }
+  //    advanceUntilIdle()
+  //
+  //    assertEquals(actual = viewModel.observe(ID_A).first(), expected = TRANSACTION_A)
+  //    assertEquals(actual = viewModel.observe(ID_B).first(), expected = TRANSACTION_B)
+  //    assertEquals(actual = viewModel.observe(ID_C).first(), expected = TRANSACTION_C)
+  //  }
 
   @DependencyGraph(
     scope = AppScope::class,
