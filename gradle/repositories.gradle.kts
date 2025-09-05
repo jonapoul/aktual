4ef7206857a6d5cbfb53bd8e827d1a6783a75a39
Jon Poulton <jpoulton@pm.me>
2025-09-05 09:12:00 +0100

    Bump sqldelight

---
+++ gradle/repositories.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+  repositories {
+    google {
+      mavenContent {
+        includeGroupByRegex(".*android.*")
+        includeGroupByRegex(".*google.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
+    mavenLocal()
+  }
+}
+
+dependencyResolutionManagement {
+  repositories {
+    google {
+      mavenContent {
+        includeGroupByRegex(".*android.*")
+        includeGroupByRegex(".*google.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
+    mavenLocal()
+  }
+}
