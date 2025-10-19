97a1bb5093bbbf92a32e522a3f30aaf3545dc9f1
Jon Poulton <jpoulton@pm.me>
2025-10-19 21:01:12 +0100

    Check for ktlint (#598)

--- scripts/ktlintFormat.sh
+++ scripts/ktlintFormat.sh
@@ -3,4 +3,6 @@
 SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR/.." || exit
 
+command -v ktlint >/dev/null 2>&1 || { echo "ktlint not found"; exit 1; }
+
 ktlint '**/*.kt' '**/*.kts' '!**/build/**' --color --format
