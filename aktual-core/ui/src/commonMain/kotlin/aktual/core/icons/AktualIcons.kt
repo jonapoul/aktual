7b20aefa0e35b011e6ff3dafa1ff85904a6f859a
Jon Poulton <jpoulton@pm.me>
2025-11-17 22:33:28 +0000

    more

--- aktual-core/ui/src/commonMain/kotlin/aktual/core/icons/AktualIcons.kt
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright 2025 Jon Poulton
- * SPDX-License-Identifier: Apache-2.0
- */
-package aktual.core.icons
-
-object AktualIcons
--- aktual-core/ui/src/commonMain/kotlin/aktual/core/ui/Tags.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2025 Jon Poulton
- * SPDX-License-Identifier: Apache-2.0
- */
-package aktual.core.ui
-
-object Tags {
-  const val PrimaryTextButtonWithLoading = "PrimaryTextButtonWithLoading:button"
-}
