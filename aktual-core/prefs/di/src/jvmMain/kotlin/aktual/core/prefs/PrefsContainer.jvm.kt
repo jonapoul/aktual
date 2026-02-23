70df5ef06ec4c131e33ec25e0a8e0e57dae09baa
Jon Poulton <jpoulton@pm.me>
2026-02-23 16:41:35 +0000

    Regenerate

--- aktual-core/prefs/di/src/jvmMain/kotlin/aktual/core/prefs/PrefsContainer.jvm.kt
+++
@@ -1,8 +0,0 @@
-package aktual.core.prefs
-
-import dev.zacsweers.metro.BindingContainer
-
-@BindingContainer
-actual interface PrefsContainer {
-  // TBC
-}
--- aktual-core/prefs/impl/README.md
+++ aktual-core/prefs/impl/README.md
@@ -3,5 +3,5 @@
 <!--region chart-->
 ![chart](atlas/chart.png)
 
-![legend](../../atlas/legend.png)
+![legend](../../../atlas/legend.png)
 <!--endregion-->
