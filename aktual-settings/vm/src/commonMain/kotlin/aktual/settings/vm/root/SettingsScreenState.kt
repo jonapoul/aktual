59940e2adf08f4ddc411f9a60707a7fd4c6bf361
Jon Poulton <jpoulton@pm.me>
2026-03-08 11:29:37 +0000

    Update SettingsScreenState.kt

--- aktual-settings/vm/src/commonMain/kotlin/aktual/settings/vm/root/SettingsScreenState.kt
+++ aktual-settings/vm/src/commonMain/kotlin/aktual/settings/vm/root/SettingsScreenState.kt
@@ -1,5 +1,7 @@
 package aktual.settings.vm.root
 
 import aktual.settings.vm.BooleanPreference
+import androidx.compose.runtime.Immutable
 
+@Immutable
 data class SettingsScreenState(val showBottomBar: BooleanPreference)
