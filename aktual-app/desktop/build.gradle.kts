1020c3df2f3465daf056da5443d3437643a3486a
Jon Poulton <jpoulton@pm.me>
2026-03-10 20:02:53 +0000

    Update build.gradle.kts

--- aktual-app/desktop/build.gradle.kts
+++ aktual-app/desktop/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 val gitVersionDate =
   providers.gitVersionCode().map { seconds ->
     val date = Instant.ofEpochSecond(seconds.toLong()).atZone(ZoneOffset.UTC).toLocalDate()
-    "%02d.%02d.%02d".format(Locale.getDefault(), date.year % 100, date.monthValue, date.dayOfMonth)
+    "%02d.%02d.%02d".format(Locale.ROOT, date.year % 100, date.monthValue, date.dayOfMonth)
   }
 
 nucleus {
