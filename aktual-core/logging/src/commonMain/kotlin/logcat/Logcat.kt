a332f38db1ffe210aa9a05aab57ef58f9f066a78
Jon Poulton <jpoulton@pm.me>
2026-03-19 20:44:49 +0000

    Fix clashing function signatures?

--- aktual-core/logging/src/commonMain/kotlin/logcat/Logcat.kt
+++ aktual-core/logging/src/commonMain/kotlin/logcat/Logcat.kt
@@ -5,73 +5,73 @@ package logcat
 object logcat {
   context(subject: Any)
   inline fun v(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.VERBOSE, tag, t, message)
+    combined1(LogPriority.VERBOSE, tag, t, message)
 
   context(subject: Any)
   inline fun d(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.DEBUG, tag, t, message)
+    combined1(LogPriority.DEBUG, tag, t, message)
 
   context(subject: Any)
   inline fun i(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.INFO, tag, t, message)
+    combined1(LogPriority.INFO, tag, t, message)
 
   context(subject: Any)
   inline fun w(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.WARN, tag, t, message)
+    combined1(LogPriority.WARN, tag, t, message)
 
   context(subject: Any)
   inline fun e(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ERROR, tag, t, message)
+    combined1(LogPriority.ERROR, tag, t, message)
 
   context(subject: Any)
   inline fun wtf(tag: String? = null, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ASSERT, tag, t, message)
+    combined1(LogPriority.ASSERT, tag, t, message)
 
   context(subject: Any)
   inline fun v(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.VERBOSE, tag = null, t, message)
+    combined1(LogPriority.VERBOSE, tag = null, t, message)
 
   context(subject: Any)
   inline fun d(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.DEBUG, tag = null, t, message)
+    combined1(LogPriority.DEBUG, tag = null, t, message)
 
   context(subject: Any)
   inline fun i(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.INFO, tag = null, t, message)
+    combined1(LogPriority.INFO, tag = null, t, message)
 
   context(subject: Any)
   inline fun w(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.WARN, tag = null, t, message)
+    combined1(LogPriority.WARN, tag = null, t, message)
 
   context(subject: Any)
   inline fun e(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ERROR, tag = null, t, message)
+    combined1(LogPriority.ERROR, tag = null, t, message)
 
   context(subject: Any)
   inline fun wtf(t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ASSERT, tag = null, t, message)
+    combined1(LogPriority.ASSERT, tag = null, t, message)
 
   inline fun v(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.VERBOSE, tag, t, message)
+    combined2(LogPriority.VERBOSE, tag, t, message)
 
   inline fun d(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.DEBUG, tag, t, message)
+    combined2(LogPriority.DEBUG, tag, t, message)
 
   inline fun i(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.INFO, tag, t, message)
+    combined2(LogPriority.INFO, tag, t, message)
 
   inline fun w(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.WARN, tag, t, message)
+    combined2(LogPriority.WARN, tag, t, message)
 
   inline fun e(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ERROR, tag, t, message)
+    combined2(LogPriority.ERROR, tag, t, message)
 
   inline fun wtf(tag: String, t: Throwable? = null, message: () -> String) =
-    combined(LogPriority.ASSERT, tag, t, message)
+    combined2(LogPriority.ASSERT, tag, t, message)
 
   @Deprecated("Not intended to be used directly")
   context(subject: Any)
-  inline fun combined(priority: LogPriority, tag: String?, t: Throwable?, message: () -> String) {
+  inline fun combined1(priority: LogPriority, tag: String?, t: Throwable?, message: () -> String) {
     subject.logcat(priority, tag, message)
     if (t != null) {
       subject.logcat(priority, tag) { t.asLog() }
@@ -79,7 +79,7 @@ object logcat {
   }
 
   @Deprecated("Not intended to be used directly")
-  inline fun combined(priority: LogPriority, tag: String?, t: Throwable?, message: () -> String) {
+  inline fun combined2(priority: LogPriority, tag: String?, t: Throwable?, message: () -> String) {
     logcat(priority, tag, message)
     if (t != null) {
       logcat(priority, tag) { t.asLog() }
