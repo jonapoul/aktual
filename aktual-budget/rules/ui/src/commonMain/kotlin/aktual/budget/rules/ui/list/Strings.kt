7bfa120ef53559943fec65ce88b44528f3b815bb
Jon Poulton <jpoulton@pm.me>
2026-04-04 16:34:50 +0100

    format

--- aktual-budget/rules/ui/src/commonMain/kotlin/aktual/budget/rules/ui/list/Strings.kt
+++ aktual-budget/rules/ui/src/commonMain/kotlin/aktual/budget/rules/ui/list/Strings.kt
@@ -157,8 +157,7 @@ internal fun rememberConditionText(
                     isPrivacyEnabled = privacy,
                   )
               )
-            }
-            else {
+            } else {
               append(value.content)
             }
           }
