920de7729a4e56a2c987ae5a23bd5daa2f804d8c
Jon Poulton <jpoulton@pm.me>
2026-04-26 09:49:08 +0100

    KeyboardType

--- aktual-budget/rules/ui/src/commonMain/kotlin/aktual/budget/rules/ui/edit/pickers/AmountTextField.kt
+++ aktual-budget/rules/ui/src/commonMain/kotlin/aktual/budget/rules/ui/edit/pickers/AmountTextField.kt
@@ -29,7 +29,6 @@ import aktual.core.ui.bareIconButton
 import aktual.core.ui.formattedString
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.minimumInteractiveComponentSize
@@ -41,7 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.KeyboardType.Companion.NumberPassword
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
@@ -49,7 +48,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
-import androidx.compose.ui.unit.dp
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonPrimitive
@@ -112,7 +110,7 @@ internal fun AmountTextField(
         onClear = { onValueChange(JsonNull) },
       )
     },
-    keyboardOptions = KeyboardOptions(keyboardType = NumberPassword),
+    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
     singleLine = true,
     textStyle = textStyle,
     visualTransformation =
