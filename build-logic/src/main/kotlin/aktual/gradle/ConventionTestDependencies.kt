50e78857b0612cf5627ca83fa724564c22609be6
Jon Poulton <jpoulton@pm.me>
2026-08-30 09:46:37 +0100

    Update dependency dev.jonpoulton.blueprint to v2.4.2

--- build-logic/src/main/kotlin/aktual/gradle/ConventionTestDependencies.kt
+++ build-logic/src/main/kotlin/aktual/gradle/ConventionTestDependencies.kt
@@ -11,17 +11,15 @@ import org.gradle.api.Project
 
 class ConventionTestDependencies : ProjectPlugin {
   override fun Project.applyTo() {
-    val testImplementation = configurations.findByName("testImplementation")
+    if ("testImplementation" !in configurations.names) return
     val isAndroid = project.plugins.any { it is AndroidBasePlugin }
 
     dependencies {
-      testImplementation?.let { testImplementation ->
-        testLibraries.forEach { lib -> testImplementation(lib) }
+      testLibraries.forEach { lib -> "testImplementation"(lib) }
 
-        if (isAndroid) {
-          androidTestLibraries.forEach { lib -> testImplementation(lib) }
-          "debugImplementation"(libs["androidx.test.monitor"])
-        }
+      if (isAndroid) {
+        androidTestLibraries.forEach { lib -> "testImplementation"(lib) }
+        "debugImplementation"(libs["androidx.test.monitor"])
       }
     }
   }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ pluginManagement {
 plugins {
   id("com.android.application") version "9.3.2" apply false
   id("com.android.kotlin.multiplatform.library") version "9.3.2" apply false
-  id("dev.jonpoulton.blueprint") version "2.4.1" apply false
+  id("dev.jonpoulton.blueprint") version "2.4.2" apply false
   id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
   id("org.jetbrains.kotlin.multiplatform") version "2.4.10" apply false
 
