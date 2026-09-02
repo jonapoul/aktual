b2ed71073655642d1fcb6845a585f61ce7215a49
Jon Poulton <jpoulton@pm.me>
2026-09-02 18:14:35 +0100

    Fix CI

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -4,6 +4,8 @@ rootProject.name = "build-logic"
 
 apply(from = "../gradle/repositories.gradle.kts")
 
+includeBuild("../compiler-plugin")
+
 dependencyResolutionManagement {
   versionCatalogs { register("libs") { from(files("../gradle/libs.versions.toml")) } }
 }
